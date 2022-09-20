5b3828836dc9dd1f7e7a494cfc5356eae094db3d
Júlia Fernandes Alves <juliafealves@gmail.com>
2022-09-20 16:34:40 -0300

    Adicionado método utilitário para remover caracteres especiais e idiomáticos.

--- src/sethe/util/StringUtils.java
+++ src/sethe/util/StringUtils.java
@@ -1,16 +1,50 @@
 package sethe.util;
 
-public class StringUtils {
-	public static boolean isEmpty(String s) {
-		if(s == null)
-			return true;
-		return s.trim().equals("");
-	}
+import java.nio.charset.Charset;
 
-	public static String stringSql(String s) {
-		if(s != null) {
-			return s.replaceAll("'", "''");
-		}
-		return null;
-	}
+public class StringUtils {
+
+  public static boolean isEmpty(String s) {
+    if (s == null) return true;
+    return s.trim().equals("");
+  }
+
+  public static String stringSql(String s) {
+    if (s != null) {
+      return s.replaceAll("'", "''");
+    }
+    return null;
+  }
+
+  public static String clearString(String string) {
+    int countSpace = 0;
+    String stringCleaned = "";
+
+    for (int i = 0; i < string.length(); i++) {
+      Character character = string.charAt(i);
+
+      if (Charset.forName("US-ASCII").newEncoder().canEncode(character)) {
+        String characterCleaned =
+          (character + "").replaceAll(
+              "[+,'!@#$&\\*()\\[\\]\\-/\\\\:;\\.\\?|]",
+              ""
+            );
+
+        if (characterCleaned.isEmpty()) continue;
+
+        if (Character.isWhitespace(character)) {
+          countSpace++;
+        } else {
+          countSpace = 0;
+        }
+
+        if (countSpace <= 1) {
+          stringCleaned += character + "";
+        }
+      }
+    }
+    stringCleaned = stringCleaned.trim();
+
+    return stringCleaned;
+  }
 }
